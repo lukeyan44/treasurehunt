82ab58aa67243495e39f5c0382b3c30feb6308eb
lukeyan <gumpagain@gmail.com>
2018-02-09 00:42:45 +0800

    question image

--- www/js/login.js
+++ www/js/login.js
@@ -665,6 +665,11 @@ function showQuestionWindow(q){
 	var token = new Date().getTime();
 
 	var html = '<div id="w'+token+'" class="popuptext" style="width:'+$(window).width()+'px;height:'+($(window).height())+'px;"><div class="popuptext-inner" style="margin-left:10px;margin-right:10px;">';
+	
+	if(q.image){
+		html += '<div><img style="width:100%;height:auto;" src="'+q.image+'"></div>';
+	}
+	
 	html += q.window1 ? q.window1 : '';
 	html += '<br/><h3 class="mapqtitle">'+q.title+'</h3><ol class="mapanswers">';
 	for(var i in q.answers){
